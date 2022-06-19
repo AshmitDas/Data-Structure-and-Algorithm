84c57e8cebfb2befadd0111ef4d37e151fed9a9b
Ashmit <ashmit454@gmail.com>
2022-06-19 12:28:41 +0530

    fisrt commit

---
+++ LinkedList.py
@@ -0,0 +1,12 @@
+class LinkedList:
+    def __init__(self, value) -> None:
+        pass
+
+    def append(self, value):
+        pass
+
+    def prepend(self, value):
+        pass
+
+    def insert(self, index, value):
+        pass
