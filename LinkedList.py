1ba64403546c8b9ffb54f9aecdbeea573d95caa8
Ashmit <ashmit454@gmail.com>
2022-06-20 12:14:37 +0530

    updated LL

--- LinkedList.py
+++ LinkedList.py
@@ -1,6 +1,3 @@
-from pickle import NONE
-
-
 class Node:
     def __init__(self, value) -> None:
         self.value = value
@@ -17,16 +14,13 @@ class LinkedList:
 
     def append(self, value):
         new_node = Node(value)
-
         if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
-        
         self.length += 1
-
         return True
 
 
@@ -44,11 +38,63 @@ class LinkedList:
         if self.length == 0:
             self.head = None
             self.tail = None
+        return temp.value
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head 
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
         return temp
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head 
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+        
+
+    
     def print_list(self) -> None:
         temp = self.head
-
         while temp is not None:
             print(temp.value)
             temp = temp.next
@@ -57,8 +103,14 @@ class LinkedList:
 
 alinkedlist = LinkedList(3)
 
-alinkedlist.append(1)
+alinkedlist.append(2)
+alinkedlist.append(8)
+alinkedlist.append(4)
 
-print(alinkedlist.pop())
-print(alinkedlist.pop())
-print(alinkedlist.pop())
+alinkedlist.insert(2,90)
+
+alinkedlist.print_list()
+
+# print(alinkedlist.pop())
+# print(alinkedlist.pop())
+# print(alinkedlist.pop())
--- test.py
+++ test.py
@@ -1,15 +1,32 @@
-head = {
-    'value': 11,
-    'next': {
-        'value': 3,
-        'next': {
-            'value':23,
-            'next': {
-                'value' : 7,
-                'next' : None
-            }
-        }
-    }
-}
+# head = {
+#     'value': 11,
+#     'next': {
+#         'value': 3,
+#         'next': {
+#             'value':23,
+#             'next': {
+#                 'value' : 7,
+#                 'next' : None
+#             }
+#         }
+#     }
+# }
 
-print(head['next']['next']['value'] )
+# print(head['next']['next']['value'] )
+
+    #     if self.length == 0:
+    #         return None
+    #     pre = self.head
+    #     temp = self.head
+    #     while temp.next is not None:
+    #         pre = temp
+    #         temp = temp.next
+    #     self.tail = pre
+    #     self.tail.next = None
+    #     self.length -= 1
+    #     if self.length == 0:
+    #         self.head = None
+    #         self.tail = None
+    #     return temp
+
+
