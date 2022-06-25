ce57af3ce09c27862acbe54077e3942c1fc41e51
Ashmit <ashmit454@gmail.com>
2022-06-25 11:59:39 +0530

    added Binary Search Tree

---
+++ BST.py
@@ -0,0 +1,22 @@
+class Node:
+
+    def __init__(self, value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+
+    def __init__(self) -> None:
+        self.root = None
+
+
+
+
+
+
+
+abst = BinarySearchTree()
+
+print(abst.root)
